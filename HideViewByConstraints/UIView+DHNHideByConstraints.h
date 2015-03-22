68fd8f9dc41e651dfba313c690aa7ffbeacd991c
Andreas Günther <mailaguenther@gmail.com>
2015-03-22 23:13:09 +0100

    podspec and documentation

---
+++ HideViewByConstraints/UIView+DHNHideByConstraints.h
@@ -0,0 +1,30 @@
+//
+//  UIVibrancyEffect+DHNHideByConstraints.h
+//  HideViewByConstraints
+//
+//  Created by Andreas on 19.03.15.
+//  Copyright (c) 2015 dreyhomenet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ *  Category to hide the view and its consuming space on its superview.
+ */
+@interface UIView (DHNHideByConstraints)
+
+/**
+ *  Hides or shows the view by changing its horizontal constraints and its subviews.
+ *
+ *  @param hide YES to hide the view, NO to show the view.
+ */
+- (void)hideHorizontal:(BOOL)hide;
+
+/**
+ *  Hides or shows the view by changing its vertical constraints and its subviews.
+ *
+ *  @param hide YES to hide the view, NO to show the view.
+ */
+- (void)hideVertical:(BOOL)hide;
+
+@end
